04c84fda99b8e85cfd512db537b5d85c44747517
jairo5200 <jairo5200@gmail.com>
2024-12-11 14:41:00 -0500

    agregamos el proxy de registros

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use('/tienda',createProxyMiddleware({
+    target: 'http://localhost:3001',
+    changeOrigin: true,
+    pathRewrite: {'^/tienda': ''}
+}));
+
+app.use('/registros',createProxyMiddleware({
+    target: 'http://localhost:3002',
+    changeOrigin: true,
+    pathRewrite: {'^/registros': ''}
+}));
+
 
 app.listen(PORT, () => {
     console.log(`api gateway listen on port: ${PORT}`)
